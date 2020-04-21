704915a511807d515c649c8e95ef86158d57612e
jorgecollazos <jorge.collazos@utec.edu.pe>
2020-04-20 22:32:50 -0500

    Contenido estático y dinámico

--- web/server.py
+++ web/server.py
@@ -8,14 +8,29 @@ db = connector.Manager()
 engine = db.createEngine()
 
 app = Flask(__name__)
-
+@app.route('/palindrome/<palabra>')
+def palindrome(palabra):
+    txt = palabra
+    if(txt == txt[::-1]):
+        txt_re = palabra + " es palindromo"
+        return(txt_re)
+    else:
+        txt_re = palabra + " es no palindromo"
+        return(txt_re)
+@app.route('/multiplo/<numero1>/<numero2>')
+def multiplo(numero1, numero2):
+    n1 = int(numero1)
+    n2 = int(numero2)
+    if(n1 % n2 == 0):
+        txt_res = str(n1) + " es múltiplo de " + str(n2)
+        return txt_res
+    else:
+        txt_res = str(n1) + " es no múltiplo de " + str(n2)
+        return txt_res
 
 @app.route('/static/<content>')
 def static_content(content):
     return render_template(content)
-
-
-
 
 if __name__ == '__main__':
     app.secret_key = ".."
